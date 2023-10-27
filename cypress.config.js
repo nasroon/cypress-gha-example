0bbdce645525b60332678275baa2f26145c262e0
nasroon <nasroones@gmail.com>
2023-10-27 20:46:07 +0700

    test paralel

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,11 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: '9hsiyh',
+  projectId: "9hsiyh",
+  // ket: "64cfc106-94a4-4048-9b94-6e2ff4a0dbb0",
+  video: true,
+  videoCompression: 32,
+  trashAssetsBeforeRuns: false,
   e2e: {
     baseUrl: "https://example.cypress.io",
     setupNodeEvents(on, config) {
