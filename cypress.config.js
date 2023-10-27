0678e409ea724c679ea186cecfe1f3234131d0e7
nasroon <nasroones@gmail.com>
2023-10-27 20:28:32 +0700

    update test paralel

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: '9hsiyh',
   e2e: {
     baseUrl: "https://example.cypress.io",
     setupNodeEvents(on, config) {
